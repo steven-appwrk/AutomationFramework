14d5a435710ad7fdeaecb9f657e6f7f655915a53
Steven <steven.s@appwrk.com>
2023-02-06 16:52:07 +0530

    Automation FrameWork suite for APPWRK

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,46 @@
+package base;
+
+import org.testng.annotations.BeforeMethod;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+
+	public static WebDriver driver;
+	public static Properties prop = new Properties();
+	public static Properties loc = new Properties();
+	public static FileReader fr;
+	public static FileReader fr1;
+
+	@BeforeMethod
+	@BeforeTest
+	public void setUp() throws IOException {
+
+		if (driver == null) {
+			fr = new FileReader(System.getProperty("user.dir") + "/src/test/resources/configfiles/config.properties");
+			fr1 = new FileReader(System.getProperty("user.dir") + "/src/test/resources/configfiles/locator.properties");
+			prop.load(fr);
+			loc.load(fr1);
+		}
+
+		if (prop.getProperty("browser").equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get(prop.getProperty("testurl"));
+		} else if (prop.getProperty("browser").equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+			driver.get(prop.getProperty("testurl"));
+		}
+	}
+}
---
+++ src/test/java/testCases/BrokenLinks.java
@@ -0,0 +1,43 @@
+package testCases;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+
+public class BrokenLinks extends BaseTest {
+
+
+	public void brokenLinks() {
+
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		System.out.println("Total number of Links are :- " + links.size());
+		for (int i = 0; i < links.size(); i++) {
+			WebElement element = links.get(i);
+			String url = element.getAttribute("href");
+			verifyLink(url);
+		}
+
+	}
+
+	public void verifyLink(String urlLink) {
+		try {
+			URL link = new URL(urlLink);
+			HttpURLConnection httpConn = (HttpURLConnection) link.openConnection();
+			httpConn.setConnectTimeout(2000);
+			httpConn.connect();
+			if (httpConn.getResponseCode() == 404) {
+				System.out.println(urlLink + " - " + httpConn.getResponseMessage() + "is a broken link");
+			} else {
+				System.out.println(urlLink + " - " + httpConn.getResponseMessage());
+			}
+		} catch (Exception e) {
+		}
+	}
+
+}
